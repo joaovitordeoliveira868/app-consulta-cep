60ee97c4d0073dfc56900953cef4b88e1f4194d7
joaovitoroliveira-cpu <joaovitor636073@gmail.com>
2025-06-15 02:33:44 -0300

    Criada a estrutura inicial do projeto, organizando as pastas principais: Components, Pages, Services e Home, para manter uma arquitetura limpa e escalável. Também foi implementada a tela Home, contendo uma mensagem simples de teste: Olá, mundo. Além disso, foram adicionadas as bibliotecas essenciais para o projeto, incluindo o Axios (para requisições HTTP) e as bibliotecas do React Navigation para gerenciar a navegação entre telas.

---
+++ app-cep/src/pages/Home/styles.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#E8F0FE',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    color: '#333',
+  },
+});
